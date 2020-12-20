af682dcf2cbcb4b383d96301391f5cef186574f9
Sara Green <smg105c@gmail.com>
2020-12-20 10:25:45 -0800

    created bookmarks api with GET POST and DELETE routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
 const app = express()
 
@@ -14,10 +15,26 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(bookmarksRouter)
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path" ${req.path}`)
+        return res
+            .status(401)
+            .json({ error: 'Unauthorized request' })
+    }
+    next()
+})
+
 
 app.get('/', (req, res) => {
-    res.send('Hello, boilerplate!')
+    res.send('Bookmarks App!')
 })
+
 
 app.use(function errorHandler(error, req, res, next) {
     let response
